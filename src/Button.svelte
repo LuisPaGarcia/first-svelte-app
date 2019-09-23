1ab2db9b00c55e3dbb4f4254b736bf060f035082
LuisPa <diganluispa@gmail.com>
2019-09-22 20:05:02 -0600

    first commit

---
+++ src/Button.svelte
@@ -0,0 +1,17 @@
+<style>
+  button {
+    font: inherit;
+    padding: 0.15rem 0.5rem;
+    background: #d10057;
+    color: white;
+    cursor: pointer;
+  }
+  button:hover,
+  button:active {
+    box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
+  }
+</style>
+
+<button on:click>
+  <slot />
+</button>
